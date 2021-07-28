7dbf8a6b18f8dae028990ba756ea2e23dfa93e30
Anderson Lopes <anderson.lopes.2010@gmail.com>
2021-07-28 23:02:15 +0200

    Includes javadocs.

--- src/main/java/com/lopes/emailms/EmailMsApplication.java
+++ src/main/java/com/lopes/emailms/EmailMsApplication.java
@@ -3,9 +3,17 @@ package com.lopes.emailms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * The type Email ms application.
+ */
 @SpringBootApplication
 public class EmailMsApplication {
 
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
     public static void main(String[] args) {
         SpringApplication.run(EmailMsApplication.class, args);
     }
